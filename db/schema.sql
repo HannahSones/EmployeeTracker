865f74acbeddd26b1277754214ac2137d7d2d033
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-08 20:16:38 +0000

    Creating the three tables for MySql

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_list_db;
+
+CREATE DATABASE employee_list_db;
+
+USE employee_list_db;
+
+-- Creating department table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creating role table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Creating employee table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
--- server.js
+++ server.js
@@ -2,4 +2,4 @@
 const mysql = require("mysql");
 const connection = require("./db/db.js")
 const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+const cTable = require("console.table");

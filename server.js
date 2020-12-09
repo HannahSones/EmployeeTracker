52f99eed2132a780d3de812e1ef2853d3b501532
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2020-12-09 22:48:33 +0000

    Add employee function working

--- server.js
+++ server.js
@@ -73,7 +73,8 @@ function mainMenu() {
 }
 
 
-// View all employees 
+// Functions to view all employees, roles and departments
+
 function viewAllEmp(){
     connection.query("SELECT * FROM employee", function(err, result) {
         if (err) console.log('Error in query', err);
@@ -82,7 +83,7 @@ function viewAllEmp(){
     })
 }
 
-// View all roles
+
 function viewAllRoles(){
     connection.query("SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;", function(err, result) {
         if (err) console.log('Error in query', err);
@@ -91,11 +92,93 @@ function viewAllRoles(){
     })
 }
 
-// View all departments
+
 function viewAllDepts(){
     connection.query("SELECT * FROM department;", function(err, result) {
         if (err) console.log('Error in query', err);
         else console.table(result);
         mainMenu()
     })
+}
+
+
+// Functions to add employees, roles and departments
+
+function addEmp() {
+
+    let roleArr = [];
+    function selectRole() {
+      connection.query("SELECT * FROM role", function(err, res) {
+        if (err) throw err
+        for (var i = 0; i < res.length; i++) {
+          roleArr.push(res[i].title);
+        }
+      })
+      return roleArr;
+    }
+    
+    let managerArr = [];
+    function selectManager() {
+      connection.query("SELECT first_name, last_name FROM employee WHERE manager_id IS NULL", function(err, res) {
+        if (err) throw err
+        for (var i = 0; i < res.length; i++) {
+          managerArr.push(res[i].first_name);
+        }
+    
+      })
+      return managerArr;
+    }
+
+        inquirer.prompt([
+            {
+              name: "firstName",
+              type: "input",
+              message: "Enter their first name "
+            },
+            {
+              name: "lastName",
+              type: "input",
+              message: "Enter their last name "
+            },
+            {
+              name: "role",
+              type: "list",
+              message: "Please select their role? ",
+              choices: selectRole()
+            },
+            {
+                name: "choice",
+                type: "list",
+                message: "Please select their manager?",
+                choices: selectManager()
+            }
+        ]).then(function (answer) {
+          var roleId = selectRole().indexOf(answer.role) + 1
+          var managerId = selectManager().indexOf(answer.choice) + 1
+          connection.query("INSERT INTO employee SET ?", 
+          {
+              first_name: answer.firstName,
+              last_name: answer.lastName,
+              manager_id: managerId,
+              role_id: roleId
+              
+          }, function(err){
+              if (err) throw err
+              console.table(answer)
+              console.log("Success. Your employee has been added.")
+              mainMenu()
+          })
+    
+      })
+    }
+
+
+function addRole() {
+    
+}
+
+
+
+function addDept() {
+    
 }
